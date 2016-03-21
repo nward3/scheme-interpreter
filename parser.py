f024016238c2c745c88479de46ecfce575f51a60
Luke Garrison <lgarriso@nd.edu>
2016-03-21 00:59:16 -0400

    Parses code into sublists
    Sublists mirror the structure of the scheme code (parens)

--- parser.py
+++ parser.py
@@ -14,11 +14,37 @@ class Parser:
 
         return tokens
 
+    # returns an array of subarrays that mirrors the nested paren structure of scheme
     def createSublists(self, tokens):
-        return self.createSublistsHelper(collections.deque(tokens), [])
+        sublists = self.createSublistsHelper(collections.deque(tokens), [])
 
+        # desired contents are nested inside an outer array
+        return sublists[0]
+
+    # tokens is a deque for optimized pop(0) operation
+    # sublists is a list
     def createSublistsHelper(self, tokens, sublists):
-        print tokens
+        # base case
+        if len(tokens) == 0:
+            # no tokens left
+            print "here"
+            return sublists
+
+        # get next token and remove remove the token that is being processed from the deque of tokens to process
+        token = tokens.popleft()
+
+        # build subarray
+        if token == "(":
+            sublists.append(self.createSublistsHelper(tokens, list()))
+            # recurse in case there are multiple sublists on this "level"
+            return self.createSublistsHelper(tokens, sublists)
+        if token == ")":
+            # sublist is finished
+            return sublists
+        else:
+            # append tokens to the sublist
+            sublists.append(token)
+            return self.createSublistsHelper(tokens, sublists)
 
 
 if __name__ == "__main__":
@@ -26,4 +52,6 @@ if __name__ == "__main__":
     s = "(display (eq? 5 a))"
     print s
     tokens = p.tokenizeInput(s)
-    p.createSublists(tokens)
+    print p.createSublists(tokens)
+
+
