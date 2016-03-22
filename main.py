4867db505e9d2aa3d9f5799626f31e6d67368ccf
Luke Garrison <lgarriso@nd.edu>
2016-03-22 16:33:36 -0400

    Adds support for command line input filename
    Also includes preliminary test files
    Automated test script will come later

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import sys
 from parser import *
 from interpreter import *
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "Please specify the scheme file to run"
+        print "Example usage: python main.py [code.scm]"
+
+        sys.exit(1)
+
+    # open file and save the contents into string s
+    filename = sys.argv[1]
+    s = str()
+    try:
+        with open(filename, 'r') as f:
+            for line in f:
+                s = s + line
+    except IOError as e:
+        print "Unable to open file: " + filename
+        sys.exit(1)
+    except Exception as ex:
+        print e
+        sys.exit(1)
+
     p = Parser()
-    s = "(cdr (list 1 turkey 3 b))"
-    print s
-    tokens = p.tokenizeInput(s)
-    print p.createSublists(tokens)
     i = Interpreter()
-    result = i.interpret(p.createSublists(tokens))
+
+    # parse the input
+    tokens = p.tokenizeInput(s)
+    tokensSublists = p.createSublists(tokens)
+
+    # pass the sublists of tokens (structure mirrors the levels of parens in
+    # scheme code) to the interpreter
+    result = i.interpret(tokensSublists)
     print p.convertToParens(result)
